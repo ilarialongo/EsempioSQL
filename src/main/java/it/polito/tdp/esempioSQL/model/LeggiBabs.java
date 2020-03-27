6af42e8fbef5f18ad2a04b29e519bd5a7ff90028
ilarialongo <ilarialongo@mbp-di-ilaria.homenet.telecomitalia.it>
2020-03-27 11:09:19 +0100

    introduzione di Prepare Statement

--- src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -1,31 +1,45 @@
 package it.polito.tdp.esempioSQL.model;
 
-import java.util.List;
-
-import it.polito.tdp.esempioSQL.db.BabsDAO;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LeggiBabs {
 	
 	public void run() {
+		//1. Stringa di connessione
 		
-		BabsDAO dao = new BabsDAO() ;
+		String jdbcURL="jdbc:mysql://localhost/babs?user=root&password=Cuscinoila";
 		
-		List<Station> tutte = dao.listStation() ;
-		
-		for(Station s: tutte) {
-			System.out.println(s.getName()) ;
+		//FACTORY: creazione di una classe, senza 
+		//sapere il tipo della classe(NON posso usare new)
+		//Uso quindi un metodo fornito da un'altra classe
+		//che internamente farà new e coniscerà il tipo di classe effettivo
+		try {
+			Connection conn=DriverManager.getConnection(jdbcURL);
+			String sql= "SELECT name FROM station WHERE landmark= ? ";
+			PreparedStatement st= conn.prepareStatement(sql);
+			st.setString(1, "Palo Alto");
+			ResultSet res= st.executeQuery();
+			
+			while(res.next()) {
+				String nomeStazione= res.getString("name");
+				System.out.println(nomeStazione);
+			}
+			st.close();			
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 		
-		System.out.println("----") ;
-		List<Station> paloAlto = dao.listStationByLandmark("Palo Alto") ;
-		for(Station s: paloAlto) {
-			System.out.println(s.getName()) ;
-		}
-
+		
 	}
 	
-	public static void main(String args[]) {
-		LeggiBabs babs = new LeggiBabs() ;
+	public static void main (String args[]) {
+		LeggiBabs babs= new LeggiBabs();
 		babs.run();
 	}
 
